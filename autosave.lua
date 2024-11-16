148c2e83a4e04b8210772a40be6b0843226efbb6
Brian Jolly <brian.jolly@elastic.co>
2024-11-15 17:20:02 -0800

    Add user command

--- autosave.lua
+++ autosave.lua
@@ -1,21 +1,33 @@
-local bufnr = 37 -- :echo nvim_get_current_buf()
+-- update with :source %
+local attach_to_buffer = function(output_buffer, pattern, command)
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = vim.api.nvim_create_augroup("TjsCoolTutorial", { clear = true }),
+		pattern = pattern,
+		callback = function ()
+			local append_data = function(_, data)
+				if data then
+					vim.api.nvim_buf_set_lines(output_buffer, -1, -1, false, data)
+				end
+			end
+
+			vim.api.nvim_buf_set_lines(output_buffer, 0, -1, false, {"output of main:"})
+			vim.fn.jobstart(command, {
+				stdout_buffered = true,
+				on_stdout = append_data,
+				on_stderr = append_data,
+			})
+		end,
+	})
+end
+
+attach_to_buffer(37, "*.go", {"go", "run", "main.go"})
 
 -- :help autocmd
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = vim.api.nvim_create_augroup("TjsCoolTutorial", { clear = true }),
-	pattern = "*.go",
-	callback = function ()
-		local append_data = function(_, data)
-			if data then
-				vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-			end
-		end
-
-		vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {"output of main:"})
-		vim.fn.jobstart({"go", "run", "main.go"}, {
-			stdout_buffered = true,
-			on_stdout = append_data,
-			on_stderr = append_data,
-		})
-	end,
-})
+-- nvim_create_user_command
+vim.api.nvim_create_user_command("AutoRun", function()
+	print("autorun starts now..")
+	local bufnr = vim.fn.input "Bufnr: "
+	local pattern = vim.fn.input "Pattern: "
+	local command = vim.split(vim.fn.input "Command: ", " ")
+	attach_to_buffer(tonumber(bufnr), pattern, command)
+end, {})
